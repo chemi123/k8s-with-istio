88d5fcb372532fd57583200f4bda03c5d3270621
chemi123 <chemi123@example.com>
2023-06-25 07:36:49 +0100

    fix

--- istio-system/istio/install.sh
+++ istio-system/istio/install.sh
@@ -5,7 +5,7 @@
 # this command mainly creates istiod, istio-ingressgateway, but also creates istio-system namespace, crds for istio
 istioctl install -f istio-operator.yaml -y
 
-kustomize build | kubectl apply -f -
-
 kubectl label namespace default istio-injection=enabled
+
+kustomize build | kubectl apply -f -
 
