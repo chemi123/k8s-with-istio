4edf565eacf7294c0618b51527e5dd2c4b0fbd93
chemi123 <chemi123@example.com>
2023-06-25 06:16:16 +0100

    refactor

--- istio/install.sh
+++ istio/install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-istioctl install -f istio-operator.yaml
+# These are steps how to setup istio cluster
+
+# this command mainly creates istiod, istio-ingressgateway, but also creates istio-system namespace, crds for istio
+istioctl install -f istio-operator.yaml -y
+
+kustomize build | kubectl apply -f -
+
+kubectl label namespace default istio-injection=enabled
+
